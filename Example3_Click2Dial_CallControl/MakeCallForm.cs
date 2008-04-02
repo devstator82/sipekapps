95db6ee2ce128444160a813bb2d68e675d28a65b
sasacoh <sasacoh@c8bbff46-5045-0410-a152-1d95290f8afa>
2008-04-02 12:34:05 +0000

    Update examples because makeCallByUri method removed Update libraries for call control example to work

--- Example3_Click2Dial_CallControl/MakeCallForm.cs
+++ Example3_Click2Dial_CallControl/MakeCallForm.cs
@@ -7,33 +7,85 @@ using System.Text;
 using System.Windows.Forms;
 using Sipek.Common.CallControl;
 using Sipek.Sip;
+using Sipek.Common;
 
 namespace Example3_makeCall_CallControl
 {
+
   public partial class MakeCallForm : Form
   {
-    CCallManager manager;
+    // create factory
+    MyFactory _myFactory = new MyFactory();
+    CCallManager _manager;
 
     public MakeCallForm()
     {
       InitializeComponent();
 
       // initialize Call Control
-      manager = CCallManager.getInstance();
+      _manager = CCallManager.getInstance();
+      
       // Assign pjsipWrapper instance as abstractProxy 
-      manager.Factory.CommonProxy = CSipCommonProxy.GetInstance();
+      _myFactory.CommonProxy = CSipCommonProxy.GetInstance();
+      // assign factory
+      _manager.Factory = _myFactory;
       // initialize Call Control
-      manager.Initialize();
+      _manager.Initialize();
     }
 
     private void button1_Click(object sender, EventArgs e)
     {
       // create call
-      CStateMachine call = manager.createOutboundCall("1234");
+      CStateMachine call = _manager.createOutboundCall(textBox1.Text);
       // show the result
       int sessionId = (call == null ? -1 : call.Session); 
       label2.Text = (sessionId >= 0 ? "Success" : "Failed") + " (" + sessionId + ")";
 
+    }
+
+    /// <summary>
+    /// Simple Abstract proxy implementation
+    /// Only IVoipProxy is non nullable. We dont't need others.
+    /// </summary>
+    public class MyFactory : AbstractFactory
+    {
+      ICallLogInterface _logger = new NullCallLogger();
+      IVoipProxy _proxy = new NullVoipProxy();
+      IConfiguratorInterface _config = new NullConfigurator();
+      IMediaProxyInterface _media = new NullMediaProxy();
+
+      #region AbstractFactory Members
+
+      public ICallLogInterface CallLogger
+      {
+        get { return _logger; }
+        set { ;}
+      }
+
+      public IVoipProxy CommonProxy
+      {
+        get { return _proxy; }
+        set { _proxy = value; }
+      }
+
+      public IConfiguratorInterface Configurator
+      {
+        get { return _config; }
+        set { ; }
+      }
+
+      public IMediaProxyInterface MediaProxy
+      {
+        get { return _media; }
+        set { ; }
+      }
+
+      public ITimer createTimer()
+      {
+        return new NullTimer();
+      }
+
+      #endregion
     }
   }
 }
