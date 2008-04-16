a504f46b4982387a1f73b8ca2e777a85b01f8247
sasacoh <sasacoh@c8bbff46-5045-0410-a152-1d95290f8afa>
2008-04-16 06:47:15 +0000

    Solution properties changed. SipekSdk sources are no longer linked. Examples use sipeksdk.dll instead.

--- Example4_LocalCall_callControl/LocalCallForm.cs
+++ Example4_LocalCall_callControl/LocalCallForm.cs
@@ -26,7 +26,7 @@ namespace Example4_LocalCall_callControl
       _manager = CCallManager.getInstance();
 
       // Assign pjsipWrapper instance as abstractProxy 
-      _manager.Factory.CommonProxy = CSipCommonProxy.GetInstance(); ;
+      _manager.Factory.CommonProxy = CSipCommonProxy.GetInstance();
       // initialize Call Control
       _manager.Initialize();
       // subscribe to CallStateChanged event
@@ -35,51 +35,50 @@ namespace Example4_LocalCall_callControl
     
     void _manager_CallStateRefresh(int sessionId)
     {
-      if (this.InvokeRequired)
-      {
-        // synchronize threads
-        this.Invoke(new DCallStateRefresh(CallStateRefreshed), new object[] { sessionId });
-      }
-      else
-      {
-        CallStateRefreshed(sessionId);
-      }
+        if (this.InvokeRequired)
+        {
+          // synchronize threads
+          this.Invoke(new DCallStateRefresh(CallStateRefreshed), new object[] { sessionId });
+        }
+        else
+        {
+          CallStateRefreshed(sessionId);
+        }
     }
 
     void CallStateRefreshed(int sessionId)
     {
-      // get call instance
-      IStateMachine call = _manager.getCall(sessionId);
+        // get call instance
+        IStateMachine call = _manager.getCall(sessionId);
 
-      // sessionId belongs to an outgoing call leg
-      if (sessionId == 0)
-      {
-        textBoxOutCallState.Text = call.State.ToString();
-        textBoxOutCallState.Tag = sessionId;
+        // sessionId belongs to an outgoing call leg
+        if (sessionId == 0)
+        {
+          textBoxOutCallState.Text = call.State.ToString();
+          textBoxOutCallState.Tag = sessionId;
+        }
+        else
+          if (sessionId == 1) // incoming call leg
+          {
+            textBoxIncCallState.Text = call.State.ToString();
+            textBoxIncCallState.Tag = sessionId;
+          }
       }
-      else 
-        if (sessionId == 1) // incoming call leg
-      {
-        textBoxIncCallState.Text = call.State.ToString();
-        textBoxIncCallState.Tag = sessionId;
-      }
-    }
 
     private void buttonOutCall_Click(object sender, EventArgs e)
     {
-      // check if number of calls (allow only 1)
-      if (_manager.Count > 0) return;
+        // check if number of calls (allow only 1)
+        if (_manager.Count > 0) return;
 
-      // create call
-      IStateMachine call = _manager.createOutboundCall("sip:127.0.0.1");
-      // show the result
-      int sessionId = (call == null ? -1 : call.Session); 
-
+        // create call
+        IStateMachine call = _manager.createOutboundCall("sip:127.0.0.1");
+        // show the result
+        int sessionId = (call == null ? -1 : call.Session);
     }
 
     private void buttonIncRelease_Click(object sender, EventArgs e)
     {
-      _manager.onUserRelease((int)textBoxIncCallState.Tag);
+        _manager.onUserRelease((int)textBoxIncCallState.Tag);
     }
 
     private void buttonIncAnswer_Click(object sender, EventArgs e)
